d96f4f3c43dd33c47d91273ba12a8182fd7cbbeb
Kanin, Maxim <ms.kanin@yandex.ru>
2021-02-09 19:13:15 +0300

    Add JAVA Faker class

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.junit5.SoftAssertsExtension;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import pages.PracticeFormPage;
@@ -18,42 +19,48 @@ public class PracticeFormTests {
     @Test
     public void practiceFormTest() {
         Configuration.assertionMode = SOFT;
+        Map<String, String> settings = getExpectedSettings();
         new PracticeFormPage()
                 .openUrl()
-                .withFirstname(expectedSettings().get("Student Name").split(" ")[0])
-                .withLastname(expectedSettings().get("Student Name").split(" ")[1])
-                .withEmail(expectedSettings().get("Student Email"))
-                .withGender(expectedSettings().get("Gender"))
-                .withMobileNumber(expectedSettings().get("Mobile"))
-                .withDateOfBirth(Integer.parseInt(expectedSettings().get("Date of Birth")
+                .withFirstname(settings.get("Student Name").split(" ")[0])
+                .withLastname(settings.get("Student Name").split(" ")[1])
+                .withEmail(settings.get("Student Email"))
+                .withGender(settings.get("Gender"))
+                .withMobileNumber(settings.get("Mobile"))
+                .withDateOfBirth(Integer.parseInt(settings.get("Date of Birth")
                                 .split(",")[0]
                                 .split(" ")[0]),
-                        expectedSettings().get("Date of Birth")
+                        settings.get("Date of Birth")
                                 .split(",")[0]
                                 .split(" ")[1],
-                        expectedSettings().get("Date of Birth")
+                        settings.get("Date of Birth")
                                 .split(",")[1])
-                .withSubjects(expectedSettings().get("Subjects").split(","))
-                .withHobbies(expectedSettings().get("Hobbies").replaceAll(" ", "").split(","))
-                .withUploadedPicture(expectedSettings().get("Picture"))
-                .withAddress(expectedSettings().get("Address"))
-                .withStateAndCity(expectedSettings().get("State and City").split(" ")[0]
-                        , expectedSettings().get("State and City").split(" ")[1])
+                .withSubjects(settings.get("Subjects").split(","))
+                .withHobbies(settings.get("Hobbies").replaceAll(" ", "").split(","))
+                .withUploadedPicture(settings.get("Picture"))
+                .withAddress(settings.get("Address"))
+                .withStateAndCity(settings.get("State and City").split(" ")[0]
+                        , settings.get("State and City").split(" ")[1])
                 .submit();
-        assertPracticeForm(expectedSettings());
+        assertPracticeForm(settings);
     }
 
-    private Map<String, String> expectedSettings() {
+    private Map<String, String> getExpectedSettings() {
         Map<String, String> map = new HashMap<>();
-        map.put("Student Name", "Maksim Kanin");
-        map.put("Student Email", "qa@mail.ru");
+        Faker faker = new Faker();
+        String name = faker.name().firstName() + " " + faker.name().lastName();
+        String email = faker.internet().emailAddress();
+        String phone = faker.phoneNumber().subscriberNumber(10);
+        String address = faker.address().fullAddress();
+        map.put("Student Name", name);
+        map.put("Student Email", email);
         map.put("Gender", "Male");
-        map.put("Mobile", "9119888888");
+        map.put("Mobile", phone);
         map.put("Date of Birth", "17 November,1989");
         map.put("Subjects", "Maths, Chemistry");
         map.put("Hobbies", "Sports, Reading, Music");
         map.put("Picture", "qa.png");
-        map.put("Address", "Saint-Petersburg");
+        map.put("Address", address);
         map.put("State and City", "NCR Noida");
         return map;
     }
